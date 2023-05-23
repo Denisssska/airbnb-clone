4c1bdb0953e1b90f5996af2ea4cdc107284b8a6f
Denisssska <yarmoshkoden18m@gmail.com>
2023-05-23 14:36:56 +0300

    first commit

---
+++ src/app/components/modals/RegisterModal.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import axios from 'axios';
+import {AiFillGithub,FcGoogle} from "react-icons/all";
+
+
+const RegisterModal = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RegisterModal;
